25b6e108b0a54b0fdef439ffb62915f12c5e927f
Brandon Nicholson <91484037+bknicholson47@users.noreply.github.com>
2023-11-10 04:02:30 +0000

    Initial commit

---
+++ Software assignment/Software assignment/Parser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_assignment
+{
+    internal class Parser
+    {
+        Graphics bmG;
+
+        public Parser(Graphics g)
+        {
+            this.bmG = g;
+        }
+        public void ParseCommand (string command)
+        {
+            Console.WriteLine (command);
+            String[] commands = command.Split (' ');
+            if (commands[0] == "circle")
+            {
+                int i = int.Parse(commands[1]);
+                int f = int.Parse(commands[2]);
+                int e = int.Parse(commands[3]);
+                Circle c = new Circle(Color.Blue, i, f, e);
+                c.draw(bmG);
+            }
+        }
+    }
+}
---
+++ Software assignment/Software assignment/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using Software_assignment;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software_assignment
+{
+    class Rectangle : Shape
+    {
+        protected int width, height;
+        public Rectangle(Color colour, int x, int y, int width, int height) : base(colour, x, y)
+        {
+
+            this.width = width; //the only thingthat is different from shape
+            this.height = height;
+        }
+
+
+        public override void draw(Graphics g)
+        {
+            Pen p = new Pen(Color.Black, 2);
+            SolidBrush b = new SolidBrush(colour);
+            g.FillRectangle(b, x, y, width, height);
+            g.DrawRectangle(p, x, y, width, height);
+        }
+    }
+}
