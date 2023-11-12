6645089ef2c21b2dfd562d9fd70f155952f02842
bknicholson47 <bknicholson47@gmail.com>
2023-11-12 23:50:58 +0000

    Implemented unit test for clear command

---
+++ Unit Tests/UnitTestClear.cs
@@ -0,0 +1,26 @@
+using Software_assignment;
+using System.Drawing;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class UnitTestClear
+    { 
+        [TestMethod]
+        public void isNotNull()
+        {
+            // arrange
+            Bitmap myBitmap = new Bitmap(600, 500);
+            Graphics bmG;
+            bmG = Graphics.FromImage(myBitmap);
+            userPen pen = new userPen(Color.Black, 0, 0, 1, true);
+            Parser p = new Parser(bmG, pen);
+
+            // act
+            p.ParseCommand("clear", "", false);
+
+            // assert
+            Assert.AreNotEqual(new Bitmap(600, 500), myBitmap);
+        }
+    }
+}
---
+++ Unit Tests/UnitTestReset.cs
@@ -0,0 +1,25 @@
+using Software_assignment;
+using System.Drawing;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class UnitTestReset
+    { 
+        [TestMethod]
+        public void isNotNull()
+        {
+            // arrange
+            Bitmap myBitmap = new Bitmap(600, 500);
+            userPen pen = new userPen(Color.Black, 0, 0, 1, true);
+            Graphics bmG;
+            bmG = Graphics.FromImage(myBitmap);
+
+            // act
+            Parser p = new Parser(bmG,pen);
+
+            // assert
+            Assert.IsNotNull(p);
+        }
+    }
+}
