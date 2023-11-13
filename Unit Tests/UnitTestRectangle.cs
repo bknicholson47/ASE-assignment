136f6e7472a4b58d6dba069361f7a34ab1116b48
bknicholson47 <bknicholson47@gmail.com>
2023-11-13 00:35:20 +0000

    Implemented Rectangle unit test

--- Unit Tests/UnitTestRectangle.cs
+++ Unit Tests/UnitTestRectangle.cs
@@ -13,9 +13,10 @@ namespace Unit_Tests
             var i = 50;
             var f = 50;
             var e = 50;
+            var g = 50;
 
             // act
-            Circle c = new Circle(i, f, e);
+            Software_assignment.Rectangle c = new Software_assignment.Rectangle(i, f, e, g);
 
             // assert
             Assert.IsNotNull(c);
