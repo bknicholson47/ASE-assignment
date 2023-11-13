7c606e4af8592a4631cc162ed6ae1a06a7156737
bknicholson47 <bknicholson47@gmail.com>
2023-11-13 00:00:07 +0000

    Implemented invalidcommand unit test

--- Unit Tests/UnitTestInvalidCommand.cs
+++ Unit Tests/UnitTestInvalidCommand.cs
@@ -11,15 +11,16 @@ namespace Unit_Tests
         {
             // arrange
             Bitmap myBitmap = new Bitmap(600, 500);
-            userPen pen = new userPen(Color.Black, 0, 0, 1, true);
             Graphics bmG;
             bmG = Graphics.FromImage(myBitmap);
+            userPen pen = new userPen(Color.Black, 0, 0, 1, true);
+            Parser p = new Parser(bmG, pen);
 
             // act
-            Parser p = new Parser(bmG,pen);
+            var output = p.ParseCommand("cloer", "", false);
 
             // assert
-            Assert.IsNotNull(p);
+            Assert.AreEqual("\ncloer not recognised", output);
         }
     }
 }
